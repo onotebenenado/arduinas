1b8d9f381850c23d4f77f4a5f129334354f27178
onotebenenado <m4rt03@gmail.com>
2026-01-19 12:05:32 +0500

    anastasiiiyyaa

--- fastTest/fastTest.ino
+++ fastTest/fastTest.ino
@@ -1,19 +1,6 @@
-int BLS = A0;  // Black line sensor
 
-float setVoltBLS3 = 3.0;  // устанавливаем напряжение BLS
-float setVoltBLS5 = 5.0;
+#include <Servo.h>
 
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(BLS, INPUT);
-}
-
-void loop() {
-  // int valBLS = analogRead(BLS);  // читаем и записываем данные с датчика
-  float voltBLS = analogRead(BLS) * setVoltBLS3 / 1024.0;
-
-  Serial.print("BLS val: ");
-  Serial.print(voltBLS);
-  Serial.println("V");
-}
+// Создаем два объекта сервопривода
+Servo servo1;
+Servo servo2;
